25e97a78ec2b42b33e6a003f14cea35d6e862307
Luís Henrique <lhfreya@gmail.com>
2021-09-14 16:48:25 -0300

    Add files via upload

---
+++ algoritmosbasic/phpone.php
@@ -0,0 +1,21 @@
+<?php
+
+$x = 2000;
+$y = 1;
+
+$z = $y * $x;
+
+if ($z == 9500){
+
+    echo $z;}
+    
+elseif($z > 9500){
+    
+    echo $z;}
+
+else{
+  
+    echo "valor não foi atingido";
+}
+
+?>
