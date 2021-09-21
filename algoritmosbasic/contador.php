abb870458f99e22bc0573cb88914794a4bf49b3f
Luís Henrique <lhfreya@gmail.com>
2021-09-21 16:51:08 -0300

    Add files via upload

---
+++ algoritmosbasic/contador.php
@@ -0,0 +1,23 @@
+<?php 
+
+
+$grupoa = array("SPFC", "BARCELONA", "ACG", "BORUSSIA");
+$arrayslots = count($grupoa);
+
+for ($c = 0; $c < $arrayslots; $c++){
+
+    echo $grupoa[$c];
+    echo "\n";
+
+
+}
+
+
+
+
+
+
+
+
+
+?>
